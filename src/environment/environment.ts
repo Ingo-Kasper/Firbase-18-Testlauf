5cf433e6e4afc035819d2857bdee93cdf0f2df4a
Ingo Kasper <Djindustrie@Protonmail.com>
2024-12-11 22:19:00 +0100

    add environment.ts

---
+++ src/environment/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+      apiKey: "DEINE_API_KEY",
+      authDomain: "DEIN_AUTH_DOMAIN",
+      databaseURL: "DEIN_DATABASE_URL",
+      projectId: "DEIN_PROJECT_ID",
+      storageBucket: "DEIN_STORAGE_BUCKET",
+      messagingSenderId: "DEIN_MESSAGING_SENDER_ID",
+      appId: "DEIN_APP_ID",
+    },
+  };
