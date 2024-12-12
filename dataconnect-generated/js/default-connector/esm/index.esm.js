6cf163d2727d9307950893ba2199e641673ca5a0
Ingo Kasper <Djindustrie@Protonmail.com>
2024-12-12 13:59:16 +0100

    bis firebase deploy insterlirtt, noch problem emit Starge zu starten.

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { getDataConnect, validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'Firbase-18-Testlauf',
+  location: 'us-central1'
+};
+
